0fe0579fbaa6790f98ba68df7735903017dd03d3
36thChamber <arturcf00@gmail.com>
2022-05-10 11:03:54 -0300

    commiting

---
+++ backend/src/main/java/com/chamber/ChamberMovies/ChamberMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.chamber.ChamberMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamberMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamberMoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/chamber/ChamberMovies/ChamberMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamber.ChamberMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamberMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
