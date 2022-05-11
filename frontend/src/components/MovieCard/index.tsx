244668db806cbc74c9ab1129cc6622f1edfb0a3c
36thChamber <arturcf00@gmail.com>
2022-05-11 10:42:28 -0300

    commiting NavigationButtons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,16 +1,17 @@
 import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
 
 function MovieCard() {
-    
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
-    
-    return (
+  const movie = {
+    id: 1,
+    image:
+      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5,
+  };
+
+  return (
     <div>
       <img
         className="dsmovie-movie-card-image"
@@ -19,12 +20,14 @@ function MovieCard() {
       />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
-        <MovieScore/>
-        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        <MovieScore />
+
+        <Link to={`/form/${movie.id}`}>
+          <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        </Link>
       </div>
     </div>
   );
 }
 
 export default MovieCard;
-
