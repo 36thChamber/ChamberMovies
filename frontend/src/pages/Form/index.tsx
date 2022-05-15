756f7ab3c5907f2b68b834fd4cdda446658c0c63
36thChamber <arturcf00@gmail.com>
2022-05-15 14:58:22 -0300

    commiting show score

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,16 +1,13 @@
 import FormCard from "components/FormCard";
-import { Link, useParams } from "react-router-dom";
-import { Movie } from "types/movie";
-
-
+import { useParams } from "react-router-dom";
 
 function Form() {
 
-  const params = useParams();
+    const params = useParams();
 
-  return (
-    <FormCard movieId={`${params.movieId}`} />
-  );
+    return (
+        <FormCard movieId={`${params.movieId}`} />
+    );
 }
 
 export default Form;
