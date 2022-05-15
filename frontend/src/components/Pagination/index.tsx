0cca4951ba85230825e42c08d98b0879e019d239
36thChamber <arturcf00@gmail.com>
2022-05-15 15:11:36 -0300

    commiting pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,14 +1,20 @@
 import {ReactComponent as Arrow} from 'assets/img/arrow.svg'
+import { MoviePage } from 'types/movie';
 import './styles.css'
 
-function Pagination() {
+type Props = {
+  page: MoviePage;
+  onChange: Function;
+}
+
+function Pagination({ page } : Props) {
     return(
         <div className="dsmovie-pagination-container">
         <div className="dsmovie-pagination-box">
           <button className="dsmovie-pagination-button" disabled={true}>
             <Arrow />
           </button>
-          <p>{`${1} de ${3}`}</p>
+          <p>{`${page.number} de ${page.totalPages}`}</p>
           <button className="dsmovie-pagination-button" disabled={false}>
             <Arrow className="dsmovie-flip-horizontal" />
           </button>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -6,49 +6,49 @@ import { BASE_URL } from "utils/requests";
 import { MoviePage } from "types/movie";
 
 function Listing() {
-  const [pageNumber, setPageNumber] = useState(0);
 
-  const [page, setPage] = useState<MoviePage>({
-      content: [],
-      last: true,
-      totalPages: 0,
-      totalElements: 0,
-      size: 12,
-      number: 0,
-      first: true,
-      numberOfElements: 0,
-      empty: true
-  });
+    const [pageNumber, setPageNumber] = useState(0);
 
-  useEffect(() => {
-      axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
-          .then(response => {
-              const data = response.data as MoviePage;
-              setPage(data);
-          });
-  }, [pageNumber]);
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
 
-  const handlePageChange = (newPageNumber : number) => {
-      setPageNumber(newPageNumber);
-  }
-  
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+    }, [pageNumber]);
 
-  return (
-    <>
-        <Pagination />
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
-        <div className="container">
-            <div className="row">
-                {page.content.map(movie => (
-                    <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard movie={movie} />
-                    </div>
-                )
-                )}
+    return (
+        <>
+            <Pagination page={page} onChange={handlePageChange} />
+
+            <div className="container">
+                <div className="row">
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
+                </div>
             </div>
-        </div>
-    </>
-);
+        </>
+    );
 }
 
 export default Listing;
