e3221c27aa55ee384d94c9f2ef7efb6ab04b1fd6
36thChamber <arturcf00@gmail.com>
2022-05-15 14:06:35 -0300

    commiting first request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,38 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const [page, setPage] = useState<MoviePage>({
+      content: [],
+      last: true,
+      totalPages: 0,
+      totalElements: 0,
+      size: 12,
+      number: 0,
+      first: true,
+      numberOfElements: 0,
+      empty: true
+  });
+
+  useEffect(() => {
+      axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+          .then(response => {
+              const data = response.data as MoviePage;
+              setPage(data);
+          });
+  }, [pageNumber]);
+
+  const handlePageChange = (newPageNumber : number) => {
+      setPageNumber(newPageNumber);
+  }
+
+
   return (
     <>
       <Pagination />
